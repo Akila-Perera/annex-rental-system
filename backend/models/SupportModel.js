513ab939d288e2dcba41d600ceafb904e794e4d8
Akila-Perera <akilanvidia@gmail.com>
2026-03-29 14:23:29 +0530

    implement support mongoose modal

---
+++ backend/models/SupportModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const supportSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Support = mongoose.model('Support', supportSchema);
+
+module.exports = Support;
