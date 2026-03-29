5e1d6557c612a13f9f3be602dfeeb9e9ebe72cf6
Akila-Perera <akilanvidia@gmail.com>
2026-03-29 14:22:11 +0530

    remove comments

--- backend/server.js
+++ backend/server.js
@@ -24,14 +24,3 @@ mongoose.connect(MONGODB_URI)
     app.listen(5000, () => console.log("🚀 Server running on http://localhost:5000"));
   })
   .catch((err) => console.log("❌ MongoDB Error:", err));
-
-// ── MongoDB Connection ──────────────────────────────────
-/*mongoose
-  .connect('mongodb://localhost:27017/ITPM')
-  .then(() => {
-    console.log('✅ MongoDB Connected');
-    app.listen(5000, () => {
-      console.log('🚀 Server running on http://localhost:5000');
-    });
-  })
-  .catch((err) => console.log('❌ MongoDB Error:', err));*/
